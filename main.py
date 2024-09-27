f63c5918514398cebf9ae37dedd97ab97975d7ce
Baton <braw31ar@gmail.com>
2024-09-27 22:23:17 +0500

    ШАблон

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine,Base
+from models import Contact
+from schemas import ContactCreate, Contact as ContactSchema
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/contacts/", response_model=ContactSchema)
+def create_contact(contact: ContactCreate, db: Session = Depends(get_db)):
+    db_contact = Contact(name=contact.name, email=contact.email, phone=contact.phone)
+    db.add(db_contact)
+    db.commit()
+    db.refresh(db_contact)
+    return db_contact
+
+@app.get("/contacts/{contact_id}", response_model=ContactSchema)
+def read_contact(contact_id: int, db: Session = Depends(get_db)):
+    return db.query(Contact).filter(Contact.id == contact_id).first()
